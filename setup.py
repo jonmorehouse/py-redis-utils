b4e0ea0d825d6af4314d5c97d2c1856e3362dfdb
Jon Morehouse <morehousej09@gmail.com>
2014-01-16 17:20:37 -0800

    Fix versioning which broke systems without redis installed

--- setup.py
+++ setup.py
@@ -3,60 +3,62 @@ import os
 import sys
 
 # import utilities version etc
-from redis_utils import __version__
+with open("redis_utils/version", "r") as f: 
+    
+        version = f.read()
 
 # import setuptools as needed
 try:
-	from setuptools import setup
-	from setuptools.command.test import test as TestCommand
-	
+        from setuptools import setup
+        from setuptools.command.test import test as TestCommand
+        
 except ImportError:
 
-	from distutils.core import setup
+        from distutils.core import setup
 
 # grab our readme generated rst for this project
 with open("readme.rst", "r") as f:
 
-	long_description = f.read()
+        long_description = f.read()
 
 # grab installation dependencies
 with open("requirements.txt", "r") as f:
 
-	installation_requirements = f.read().splitlines()
+        installation_requirements = f.read().splitlines()
 
 # grab test requirements
 with open("test-requirements.txt", "r") as f:
 
-	test_requirements = f.read().splitlines()
+        test_requirements = f.read().splitlines()
 
 # setup project 
 setup(
-	name="redis-utils",
-	version = __version__,
-	description = "Redis utilities for TDD/BDD",
-	long_description = long_description,
-	url = "http://github.com/jonmorehouse/py-redis-utils",
-	author = "Jon Morehouse",
-	author_email = "morehousej09@gmail.com",
-	keywords = ["redis", "utilities"],
-	license = "MIT",
-	packages = ["redis_utils"],
-	install_requires = installation_requirements,
-	tests_require = test_requirements,
-	test_suite = "nose.collector",
+        name="redis-utils",
+        version = version,
+        description = "Redis utilities for TDD/BDD",
+        long_description = long_description,
+        url = "http://github.com/jonmorehouse/py-redis-utils",
+        author = "Jon Morehouse",
+        author_email = "morehousej09@gmail.com",
+        keywords = ["redis", "utilities"],
+        license = "MIT",
+        packages = ["redis_utils"],
+        install_requires = installation_requirements,
+        tests_require = test_requirements,
+        test_suite = "nose.collector",
 
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.5',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-	]
+        classifiers=[
+                'Development Status :: 4 - Beta',
+                'Environment :: Console',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: MIT License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.5',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.2',
+                'Programming Language :: Python :: 3.3',
+        ]
 )
